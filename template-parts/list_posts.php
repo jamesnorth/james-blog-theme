3eea9997509f8349cd9757aaa8b6064e17f820f1
James North <jamesnorth2104@gmail.com>
2022-07-24 17:12:30 +0100

    clean-up for post list

--- template-parts/list_posts.php
+++ template-parts/list_posts.php
@@ -1,10 +1,11 @@
-<div class="row>"><div class="col-lg-8 col-md-10 mx-auto">
+<div class="row>">
+<div class="col-lg-8 col-md-10 mx-auto">
 <?php
 if (have_posts()) {
-    $i = 0;
+    $post_counter = 0;
     while (have_posts()) {
-        $i++;
-        if ($i > 1) {
+        // After the first post we want to include a horizontal separator
+        if (++$post_counter > 1) {
             echo '<hr class="post-separator" aria-hidden="true" />';
         }
     
@@ -12,10 +13,12 @@ if (have_posts()) {
         get_template_part('template-parts/content', get_post_type());
     }
 
+    // output the pagination links
     echo paginate_links();
     echo "<p>&nbsp;</p>";
 } else {
     echo '<h1>Nothing here yet.</h1>';
 }
 ?>
-</div></div>
+</div> <!-- col -->
+</div> <!-- row -->
